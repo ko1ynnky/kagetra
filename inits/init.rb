88d03274996bcfb208e79255c741847cb79e1e66
ko1ynnky <koichi.okuda.dev@gmail.com>
2026-08-09 13:51:18 +0900

    Harden album image processing

--- inits/init.rb
+++ inits/init.rb
@@ -12,6 +12,9 @@ if development? then
 end
 require 'logger'
 require 'haml'
+require 'fileutils'
+require 'pathname'
+require 'tmpdir'
 
 require 'json'
 require 'base64'
@@ -22,6 +25,7 @@ require_relative '../libs/diff_match_patch-ruby/diff_match_patch.rb'
 G_DIMAPA = DiffPatchMatch.new
 require 'rmagick'
 require 'zip'
+require_relative './album_image_security'
 
 require 'nokogiri'
 
